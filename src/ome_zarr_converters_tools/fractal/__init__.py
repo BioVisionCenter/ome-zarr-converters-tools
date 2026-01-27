b897cdd8b5f65312ef30c2a2ed24767dc4ee7cfd
lorenzo <lorenzo.cerrone@uzh.ch>
2026-01-27 15:39:30 +0100

    rename api module

--- src/ome_zarr_converters_tools/fractal/__init__.py
+++ src/ome_zarr_converters_tools/fractal/__init__.py
@@ -1,16 +1,16 @@
 """API for building OME-Zarr converters tasks for Fractal."""
 
-from ome_zarr_converters_tools.api._compute_task import (
-    ImageListUpdateDict,
-    generic_compute_task,
-)
-from ome_zarr_converters_tools.api._init_task import (
-    setup_images_for_conversion,
-)
 from ome_zarr_converters_tools.filters._filter_pipeline import (
     ImplementedFilters,
     add_filter,
     apply_filter_pipeline,
+)
+from ome_zarr_converters_tools.fractal._compute_task import (
+    ImageListUpdateDict,
+    generic_compute_task,
+)
+from ome_zarr_converters_tools.fractal._init_task import (
+    setup_images_for_conversion,
 )
 
 __all__ = [
